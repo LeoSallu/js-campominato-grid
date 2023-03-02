16832a5ecb40f2c726c106ee0c95d503d03275a4
LeoSallu <L.sallustio99@gmail.com>
2023-03-02 15:57:36 +0100

    Creazione funzioni con supporto della lezione

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
 'use strict';
 
 // Funzioni 
+function elementCreator(htmlElement,className,htmlValue){
+    const element = document.createElement(htmlElement);
+    element.classList.add(className);
+    element.innerText =htmlValue
+    return element;
+}
+function myAppendElement(containerElement , htmlElement){
+    containerElement.append(htmlElement);
+}
+
+
+
+
+// Board Box
+
+const boardBox = document.querySelector('.board');
+        
+        // Ciclo for per generare celle 
+for(let i = 1 ; i <= 100; i++){
+    const createdElement = elementCreator('div', 'cell', i);
+    myAppendElement(boardBox,createdElement);
+}
+
